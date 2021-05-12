8e541342db39e83771b013f47b5349d355fcf3ea
pavel <tominpasha@mail.ru>
2021-05-12 18:41:07 +0700

    filters #1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,6 +13,25 @@ const onClickModalOverlay = (e) => {
 	modal.classList.remove('modal_show');
 };
 
+const onClickLayerHandler = (e) => {
+	e.preventDefault();
+
+	const layerId = e.currentTarget.getAttribute('href');
+	const layer = document.querySelector(layerId);
+	layer.classList.add('layer_show');
+};
+
+const onClickLayerClose = (e) => {
+	e.preventDefault();
+
+	const layer = e.currentTarget.closest('.layer');
+	layer.classList.remove('layer_show');
+};
+
 document.querySelectorAll('a[href^="#modal"]').forEach((el) => el.addEventListener('click', onClickModalHandler));
 
 document.querySelectorAll('.modal__overlay').forEach((el) => el.addEventListener('click', onClickModalOverlay));
+
+document.querySelectorAll('a[href^="#layer"]').forEach((el) => el.addEventListener('click', onClickLayerHandler));
+
+document.querySelectorAll('.layer__close').forEach((el) => el.addEventListener('click', onClickLayerClose));
